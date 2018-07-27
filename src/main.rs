f0c8a95a5867478e3754f3b9e500930652acf16b
Антон Синицын <antony@email.su>
2018-07-27 13:38:22 +0300

    Внедряю rocket для обработки результатов формы

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,40 @@
-#![feature(plugin)]
+#![feature(plugin, custom_derive)]
 #![plugin(rocket_codegen)]
 
 extern crate rocket;
 
+use std::io;
+use std::path::{Path, PathBuf};
+
+use rocket::request::{Form, FromFormValue};
+use rocket::response::NamedFile;
+use rocket::http::RawStr;
+
+#[derive(Debug, FromForm)]
+struct FormInput<'r> {
+    checkbox: bool,
+    password: &'r RawStr,
+}
+
+#[post("/processing", data="<sink>")]
+fn processing<'r>(sink: Result<Form<'r, FormInput<'r>>, Option<String>>) -> String {
+    match sink {
+        Ok(form) => format!("{:?}", form.get()),
+        Err(Some(f)) => format!("Invalid form input: {}", f),
+        Err(None) => format!("Form input was invalid UTF8."),
+    }
+}
+
 #[get("/")]
-fn hello() -> &'static str {
-    "Hello, world!"
+fn index() -> io::Result<NamedFile> {
+    NamedFile::open("static/index.html")
+}
+
+#[get("/<file..>")]
+fn files(file: PathBuf) -> Option<NamedFile> {
+    NamedFile::open(Path::new("static/").join(file)).ok()
 }
 
 fn main() {
-    rocket::ignite().mount("/", routes![hello]).launch();
+    rocket::ignite().mount("/", routes![index, files, processing]).launch();
 }
