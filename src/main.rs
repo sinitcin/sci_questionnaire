958f8d30b8916f7185676bb5e770123302175070
Антон Синицын <antony@email.su>
2018-08-02 12:58:57 +0300

    Теперь все данные пишуться в БД

--- src/main.rs
+++ src/main.rs
@@ -26,7 +26,7 @@ pub struct DataRow<'a> {
     work_type: WorkType,
     active_work_hours: u8,
     email: &'a str,
-    points: Vec<u64>,
+    points: &'a str,
 }
 
 impl<'a> From<&'a FormInput<'a>> for DataRow<'a> {
@@ -35,28 +35,20 @@ impl<'a> From<&'a FormInput<'a>> for DataRow<'a> {
             "Полный день" => WorkType::FullDay,
             _ => WorkType::FreeDay,
         };
-        let mut arrpoint = Vec::new();
         let buffer = raw_data.points.percent_decode().expect(
             "Не смог расшифровать данные в POST запросе...",
         );
-        let v: serde_json::Value = serde_json::from_str(&buffer).expect(&format!(
+        let _v: serde_json::Value = serde_json::from_str(&buffer).expect(&format!(
             "Не смог принять массив: {}",
             buffer
         ));
-        for i in 0..23 {
-            arrpoint.push(
-                v[i][1]
-                    .as_u64()
-                    .expect("Не смог получить данные из массива..."),
-            );
-        }
         DataRow {
             age: raw_data.age,
             specialization: raw_data.specialization,
             work_type: wtype,
             active_work_hours: raw_data.active_work_hours,
             email: raw_data.email,
-            points: arrpoint,
+            points: raw_data.points,
         }
     }
 }
@@ -89,9 +81,12 @@ fn processing<'r>(param_form: Form<'r, FormInput<'r>>) -> Result<Redirect, Strin
     let decoded_mail = &RawStr::from_str(decoded_mail);
     let decoded_specialization = &*param.specialization.percent_decode().expect("");
     let decoded_specialization = &RawStr::from_str(decoded_specialization);
+    let decoded_points = &*param.points.percent_decode().expect("");
+    let decoded_points = &RawStr::from_str(decoded_points);
     let param2 = &FormInput {
         specialization: decoded_specialization,
         email: decoded_mail,
+        points: decoded_points,
         ..*param
     };
     let data = DataRow::from(param2);
@@ -136,20 +131,21 @@ pub mod database {
         let connection = ok!(sqlite::open("database.sqlite"));
         if !avail {
             ok!(connection.execute(
-                "CREATE TABLE opinions (age INTEGER, specialization TEXT, work_type INTEGER, active_work_hours INTEGER, email TEXT);"
+                "CREATE TABLE opinions (age INTEGER, specialization TEXT, work_type INTEGER, active_work_hours INTEGER, email TEXT, points TEXT);"
             ));
         }
     }
 
     pub fn store_row(data: DataRow) {
         let connection = ok!(sqlite::open("database.sqlite"));
-        let statement = "INSERT INTO opinions (age, specialization, work_type, active_work_hours, email) VALUES (?, ?, ?, ?, ?)";
+        let statement = "INSERT INTO opinions (age, specialization, work_type, active_work_hours, email, points) VALUES (?, ?, ?, ?, ?, ?)";
         let mut statement = ok!(connection.prepare(statement));
         ok!(statement.bind(1, data.age as i64));
         ok!(statement.bind(2, data.specialization));
         ok!(statement.bind(3, data.work_type as i64));
         ok!(statement.bind(4, data.active_work_hours as i64));
         ok!(statement.bind(5, data.email));
+        ok!(statement.bind(6, data.points));
         assert_eq!(ok!(statement.next()), State::Done);
     }
 }
